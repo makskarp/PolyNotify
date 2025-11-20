f5443e2c42ec85bff6bcc689ce4c0e36499944d4
Maksym Karpenko <12345ka1221@gmail.com>
2025-11-20 23:22:42 +0000

    Remove dist

--- dist/lambda/webhook.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const grammy_1 = require("grammy");
-const bot_1 = require("../bot");
-const bot = (0, bot_1.createBot)();
-// AWS Lambda handler for Telegram Webhook
-exports.handler = (0, grammy_1.webhookCallback)(bot, 'aws-lambda');
